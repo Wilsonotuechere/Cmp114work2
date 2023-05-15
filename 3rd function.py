29dafacd5faf2f3966a3565efcbbdcb2f0859dbd
kayode-vic <132939666+kayode-vic@users.noreply.github.com>
2023-05-15 15:36:39 +0100

    Create 3rd function.py

---
+++ 3rd function.py
@@ -0,0 +1,8 @@
+# Python program to print reverse number 
+
+number=int(input("Enter the number "))
+i = number
+print("number from{0}to 1".format(number))
+while(i>=1):
+    print(i,end=' ')
+    i=i-1
